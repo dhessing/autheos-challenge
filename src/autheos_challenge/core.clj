56332a366c671c4c056ab9ee0af6b06634c2fc06
dhessing <dhessing@gmail.com>
2016-07-06 20:02:04 +0200

    Fixes a bug with whole numbers

--- src/autheos_challenge/core.clj
+++ src/autheos_challenge/core.clj
@@ -4,7 +4,8 @@
 (defn number->english [n]
   (let [english (clojure.pprint/cl-format nil "~R" n)
         words (clojure.string/split english #"\s")]
-    (if (< 2 (count words))
+    (if (and (not= (rem n 100) 0)
+             (< 2 (count words)))
       (clojure.string/join " " (concat (butlast words) ["and" (last words)]))
       english)))
 
@@ -13,5 +14,7 @@
   (is (= (number->english 342) "three hundred and forty-two"))
   (is (= (number->english 115) "one hundred and fifteen"))
   (is (= (number->english 200) "two hundred"))
+  (is (= (number->english 220) "two hundred and twenty"))
+  (is (= (number->english 100000) "one hundred thousand"))
   (is (= (number->english 999999) "nine hundred ninety-nine thousand, nine hundred and ninety-nine"))
   (is (= (number->english 1000000) "one million")))
